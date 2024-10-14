eecd8a3756357d5555cff8bdf112a1ea184fab12
Ruhaan18 <raiyandalal8@gmail.com>
2024-10-14 19:20:04 +0530

    add multiply example.py

--- example.py
+++ example.py
@@ -19,3 +19,16 @@ e = d + 100
 print(f"\nResult Tensor:{e}")
 print(f"Shape of result Tensor:{e.shape}")
 print(f"Dim of Result Tensor:{e.ndim}\n")
+
+#multiplication ele-wise
+c = a * b
+print(f"\nResult Tensor:{c}")
+print(f"Shape of result Tensor:{c.shape}")
+print(f"Dim of Result Tensor:{c.ndim}\n")
+
+# broadcast rule for mutiply
+d = ct.Tensor([[1,2,3],[3,4,5]])
+e = d * 5
+print(f"\nResult Tensor:{e}")
+print(f"Shape of result Tensor:{e.shape}")
+print(f"Dim of Result Tensor:{e.ndim}\n")
