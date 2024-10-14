5db5824fb74f6e1dcfdb73dcb08361740ce052c1
Ruhaan18 <raiyandalal8@gmail.com>
2024-10-14 19:09:20 +0530

    add logic of element-wise multiplication

--- cgrad/tensor/storage.h
+++ cgrad/tensor/storage.h
@@ -13,4 +13,5 @@ void cal_stride(CTensor *tensor);
 CTensor* init_tensor(float *data, int *shape, int dim);
 void display_tensor(CTensor *tensor);
 CTensor* add_tensor(CTensor* tensor1, CTensor* tensor2);
+CTensor* mul_ele_tensor(CTensor* tensor1, CTensor* tensor2);
 #endif
