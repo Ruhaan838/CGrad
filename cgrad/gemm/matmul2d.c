65b67087de59c496c078fcd6bcd77f4b4e48fe25
Ruhaan18 <raiyandalal8@gmail.com>
2024-11-03 09:30:56 +0530

    make change the fucntion now it able to do this kind of matmul also (2,4) @ (4,)

--- cgrad/gemm/matmul2d.c
+++ cgrad/gemm/matmul2d.c
@@ -6,26 +6,31 @@
 #include "../storage/methods.h"
 
 int matmul_broadcast_shape(int dim1, int dim2, int* shape1, int* shape2, int* shape3) {
+    int max_dim = (dim1 > dim2) ? dim1 : dim2;
 
-    int max_dim = dim1 > dim2 ? dim1 : dim2;
-
-    if (shape1[dim1 - 1] != shape2[dim2 - 2]) {
+    if (dim2 == 1) {
+        if (shape1[dim1 - 1] != shape2[0]) {
+            return -1;
+        }
+    } else if (shape1[dim1 - 1] != shape2[dim2 - 2]) {
         return -1;
     }
 
     for (int i = 0; i < max_dim - 2; i++) {
-        int new_dim_a = (i >= dim1 - 2) ? 1 : shape1[dim1 - 3 - i]; 
+        int new_dim_a = (i >= dim1 - 2) ? 1 : shape1[dim1 - 3 - i];
         int new_dim_b = (i >= dim2 - 2) ? 1 : shape2[dim2 - 3 - i];
 
         if (new_dim_a != 1 && new_dim_b != 1 && new_dim_a != new_dim_b) {
             return -1;
         }
-        if (shape3 != NULL)
+        if (shape3 != NULL) {
             shape3[max_dim - 3 - i] = (new_dim_a > new_dim_b) ? new_dim_a : new_dim_b;
+        }
     }
-    if (shape3 != NULL){
-        shape3[max_dim - 2] = shape1[dim1 - 2];  
-        shape3[max_dim - 1] = shape2[dim2 - 1];  
+
+    if (shape3 != NULL) {
+        shape3[max_dim - 2] = shape1[dim1 - 2]; 
+        shape3[max_dim - 1] = (dim2 == 1) ? 1 : shape2[dim2 - 1]; 
     }
 
     return max_dim;
