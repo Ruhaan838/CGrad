893cbbeaa24779b167f18d7faa03bf05d802a640
Ruhaan18 <raiyandalal8@gmail.com>
2024-11-08 18:47:10 +0530

    add examples

--- examples/5-backward_comx_example.py
+++ examples/5-backward_comx_example.py
@@ -1,17 +1,37 @@
 import cgrad
 
-x1 = cgrad.Tensor([[1, 3, 10], [1, 3, 4]], require_grad=True)
-x2 = cgrad.Tensor([[1, 2, 10]], require_grad=True)
+x1 = cgrad.Tensor([[1, 3, 10], [1, 3, 4]], requires_grad=True)
+x2 = cgrad.Tensor([[1, 2, 10]], requires_grad=True)
 
-w1 = cgrad.Tensor([[3, 5, 10], [3, 10, 11]], require_grad=True)
-w2 = cgrad.Tensor([[6, 7, 10]], require_grad=True)
+w1 = cgrad.Tensor([[3, 5, 10], [3, 10, 11]], requires_grad=True)
+w2 = cgrad.Tensor([[6, 7, 10]], requires_grad=True)
 
-b = cgrad.Tensor([[8, 10, 10]], require_grad=True)
+b = cgrad.Tensor([[8, 10, 10]], requires_grad=True)
 
 x1w1 = x1 * w1
 x2w2 = x2 * w2
 x1w1x2w2 = x1w1 + x2w2
 n = x1w1x2w2 + b
+
+
+print(r"""
+
+        x1 ---
+               \
+                * --- x1w1 -----------
+               /                       \
+        w1 ---                          \
+                                         + ---  x1w1x2w2 ----- n
+        x2 ---                          /                     /
+               \                       /              b -----
+                * --- x2w2 -----------
+               /        
+        w2 ---          
+    
+    
+      """)
+
+
 
 print("\n","-"*10, "forward pass", "-"*10,"\n")
 print("x1:",x1)
@@ -25,7 +45,7 @@ print("x1w1x2w2:",x1w1x2w2)
 print("n:",n)
 
 print("\n","-"*10 ,"backward pass", "-"*10,"\n")
-n.backward()
+n.sum().backward()
 print("x1w1x2w2:",x1w1x2w2.grad)
 print("b:",b.grad)
 print("x1w1:",x1w1.grad)
