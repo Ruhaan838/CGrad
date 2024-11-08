ec6929998834c6d4a8232701ee9be693e8b8e283
Ruhaan18 <raiyandalal8@gmail.com>
2024-11-08 18:51:37 +0530

    make improvement in Tensor and make reable not there are no robust

--- cgrad/__init__.py
+++ cgrad/__init__.py
@@ -1,3 +1,29 @@
-from .tensor.Tensorwrapper import Tensor
+from .tensor import Tensor
 from .optium.basic_ops import rand, randrange, zeros, ones
-__all__ = ['Tensor', 'rand', 'randrange', 'zeros', 'ones']
+from .autograd import AutoGrad
+
+__all__ = ["rand", "randrange", "zeros", "ones", "AutoGrad"]
+
+def add(tensor1:Tensor, tensor2:Tensor) -> Tensor:
+    return tensor1 + tensor2
+
+def sub(tensor1:Tensor, tensor2:Tensor) -> Tensor:
+    return tensor1 - tensor2
+
+def mul(tensor1:Tensor, tensor2:Tensor) -> Tensor:
+    return tensor1 * tensor2
+
+def div(tensor1:Tensor, tensor2:Tensor) -> Tensor:
+    return tensor1 / tensor2
+
+def matmul(tensor1:Tensor, tensor2:Tensor) -> Tensor:
+    return tensor1 @ tensor2
+
+def transpose(tensor1:Tensor) -> Tensor:
+    return tensor1.transpose()
+
+def zeros_like(tensor:Tensor) -> Tensor:
+    return zeros(tensor.shape)
+
+def ones_like(tensor:Tensor) -> Tensor:
+    return ones(tensor.shape)
---
+++ cgrad/tensor.pyi
@@ -0,0 +1,110 @@
+from typing import List, Tuple, Optional
+import numpy as np
+
+from typing import List, Tuple, Optional, Iterable
+import numpy as np
+
+class Tensor:
+    """
+        Class to repesent a Tensor.
+
+        Attributes
+        ----------
+        data : List | Tuple | np.array | int | float
+            Any Iterable 
+        
+        requires_grad: Optional[bool] = False
+            if `True` the gradient caculation is happend
+
+        Methods
+        ----------
+        item:
+            return the item of the tensor in list from.
+        shape:
+            return the shape of the tensor in tuple from.
+        ndim:
+            return the dim of the tensor in int from.
+
+        add(other):
+            other: Tensor | int | float
+            add the Tensor or number.
+
+        sub(other):
+            other: Tensor | int | float
+            sub the Tensor or number.
+
+        mul(other):
+            other: Tensor | int | float
+            mul the Tensor or number.
+
+        div(other):
+            other: Tensor | int | float
+            div the Tensor or number.
+
+        pow(other):
+            other: Tensor | int | float
+            pow the Tensor or number.
+            
+        matmul(other):
+            other: Tensor
+            matrix multiplication of the Two valid shape tensor.
+        
+    """
+    def __init__(self, data: List[float] | List[int] | Tuple[float] | Tuple[int] | Iterable | int| float, requires_grad : Optional[bool] = False) -> None: ...
+    """
+            Function that initalize the tensor using List, Tuple, np.array, int or float
+            Attributes
+            ----------
+            data : list | tuple | np.array | int | float
+                Any Iterable 
+                
+            require_grad: Optional[bool] = False
+                if `True` the gradient caculation is happend
+    """
+    @property
+    def item(self) -> List[float]: ...
+    @property
+    def shape(self) -> Tuple[int]: ...
+    @property
+    def ndim(self) -> int: ...
+    
+    @property
+    def grad(self, value:Tensor) -> Tensor: ...
+    @property
+    def requires_grad(): ...
+    
+    def __add__(self, other:Tensor | int | float) -> Tensor: ...
+    def __radd__(self, other:Tensor | int| float) -> Tensor: ...
+    def __sub__(self, other:Tensor | int| float) -> Tensor: ...
+    def __rsub__(self, other:Tensor | int| float) -> Tensor: ...
+    def __mul__(self, other:Tensor | int| float) -> Tensor: ...
+    def __rmul__(self, other:Tensor | int| float) -> Tensor: ...
+    def __truediv__(self, other:Tensor | int| float) -> Tensor: ...
+    def __rtruediv__(self, other:Tensor | int| float) -> Tensor: ...
+    def __pow__(self, other:Tensor | int| float) -> Tensor: ...
+    def __matmul__(self, other:Tensor) -> Tensor: ...
+
+    def transpose(self) -> Tensor: ...
+    """
+        Transpose the tensor for all batch.
+    """
+    def backward(self, custom_grad:Optional[Tensor] = None) -> None: ...
+    """
+        Caculate the backward pass when if require_grad is True
+    """
+    def sum(self, axis:int=None, keepdims:Optional[bool]=False)-> Tensor: ...
+    """
+        Sum the over the axis with check if keepdims.
+    """
+    def mean(self, axis:int=None, keepdims:Optional[bool]=False) -> Tensor: ...
+    """
+        mean the over the axis with check if keepdims.
+    """
+    def median(self, axis:int=None, keepdims:Optional[bool]=False) -> Tensor: ...
+    """
+        median the over the axis with check if keepdims.
+    """
+    def zeros_(self) -> None: ...
+    """
+        Set the tensor values to zero.
+    """
