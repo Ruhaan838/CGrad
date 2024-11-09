ffee003d2a50da6b58af5b22b9a05af4b2b4f4f0
Ruhaan18 <raiyandalal8@gmail.com>
2024-11-09 09:44:40 +0530

    add some docs

--- cgrad/__init__.py
+++ cgrad/__init__.py
@@ -1,6 +1,7 @@
 from .tensor import Tensor
 from .optium.basic_ops import rand, randrange, zeros, ones
 from .autograd import AutoGrad, GradMode
+from typing import Optional
 
 __all__ = ["rand", "randrange", "zeros", "ones", "AutoGrad", "GradMode"]
 
@@ -19,11 +20,11 @@ def div(tensor1:Tensor, tensor2:Tensor) -> Tensor:
 def matmul(tensor1:Tensor, tensor2:Tensor) -> Tensor:
     return tensor1 @ tensor2
 
-def transpose(tensor1:Tensor) -> Tensor:
-    return tensor1.transpose()
+def transpose(tensor:Tensor) -> Tensor:
+    return tensor.transpose()
 
-def zeros_like(tensor:Tensor) -> Tensor:
-    return zeros(tensor.shape)
+def zeros_like(tensor:Tensor, requires_grad:Optional[bool]=False) -> Tensor:
+    return zeros(tensor.shape, requires_grad=requires_grad)
 
-def ones_like(tensor:Tensor) -> Tensor:
-    return ones(tensor.shape)
+def ones_like(tensor:Tensor, requires_grad:Optional[bool]=False) -> Tensor:
+    return ones(tensor.shape, requires_grad=requires_grad)
